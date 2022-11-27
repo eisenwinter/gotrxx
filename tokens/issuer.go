eisenwinter/gotrxx

commit a9cca51c9bb2e4e5fb5243f42ab1d78d4bdf48a5

refactor: extraced key loading from issuer extracted error page rendering for loginview and signup view extracted email sending form invite

tokens/issuer.go
@@ -161,68 +161,11 @@ func NewIssuer(
 	//okay this is probably the only reason and place to panic...
 	switch cfg.Algorithm {
 	case "HS256", "HS384", "HS512":
-		//direct key takes precende
-		if len(cfg.HMACSigningKey) > 0 {
-			checkForWeakHMAC(log, cfg.Algorithm, cfg.HMACSigningKey)
-			privateKey = []byte(cfg.HMACSigningKey)
-		} else if len(cfg.HMACSigningKeyFile) > 0 {
-			content, err := os.ReadFile(cfg.HMACSigningKeyFile)
-			if err != nil {
-				log.Fatal("Could not load key file", zap.String("file", cfg.HMACSigningKeyFile), zap.Error(err))
-			}
-			checkForWeakHMAC(log, cfg.Algorithm, string(content))
-
-		} else {
-			log.Fatal("No HMAC key defined, either set jwt.hmac-signing-key or jwt.hmac-signing-key-file")
-		}
-		if len(privateKey.([]byte)) > 0 {
-			var err error
-			privateKeyJwk, err = jwk.FromRaw(privateKey)
-			if err != nil {
-				log.Fatal("Unable to process symetric key")
-			}
-			options = append(
-				options,
-				jwt.WithKey(jwa.SignatureAlgorithm(cfg.Algorithm), privateKeyJwk),
-			)
-		}
+		options = loadHMACKey(cfg, log, privateKey, privateKeyJwk, options)
 	case "RS256", "RS384", "RS512":
-		if len(cfg.RSAPrivateKey) > 0 {
-			privateKey = []byte(cfg.RSAPrivateKey)
-		} else if len(cfg.RSAPRivateKeyFile) > 0 {
-			content, err := os.ReadFile(cfg.RSAPRivateKeyFile)
-			if err != nil {
-				log.Fatal("Could not load key file", zap.String("file", cfg.RSAPRivateKeyFile), zap.Error(err))
-			}
-			if len(content) == 0 {
-				log.Fatal("Read empty private key file", zap.String("file", cfg.RSAPRivateKeyFile), zap.Error(err))
-			}
-			privateKey = content
-		} else {
-			log.Fatal("No RSA private key defined, either set jwt.rsa-private-key or jwt.rsa-private-key-file")
-		}
 		var err error
-		privateKey, err = parseRSAPrivateKey(privateKey.([]byte))
-		if err != nil {
-			log.Fatal("Unable to process suplied private key", zap.Error(err))
-		}
-		if len(cfg.RSAPublicKey) > 0 {
-			publicKey = []byte(cfg.RSAPublicKey)
-		} else if len(cfg.RSAPRivateKeyFile) > 0 {
-			content, err := os.ReadFile(cfg.RSAPublicKeyFile)
-			if err != nil {
-				log.Fatal("Could not load key file", zap.String("file", cfg.RSAPublicKeyFile), zap.Error(err))
-			}
-			publicKey = content
-		} else {
-			log.Fatal("No RSA private key defined, either set jwt.rsa-public-key or jwt.rsa-public-key-file")
-		}
-		kid = fmt.Sprintf("%x", crc32.Checksum(publicKey.([]byte), crc32.IEEETable))
-		pubParsed, err := parseRSAPublicKey(publicKey.([]byte))
-		if err != nil {
-			log.Fatal("Unable to process supllied public key", zap.Error(err))
-		}
-		privateKey.(*rsa.PrivateKey).PublicKey = *pubParsed
+		var pubParsed *rsa.PublicKey
+		kid, privateKey, pubParsed = loadRSAKeys(cfg, privateKey, log, publicKey, kid)
 		privateKeyJwk, err = jwk.FromRaw(privateKey)
 		if err != nil {
 			log.Fatal("Unable to process private key")
@@ -266,6 +209,86 @@ func NewIssuer(
 		publicKey:          publicKeyJwk,
 		kid:                kid,
 	}
+}
+
+func loadRSAKeys(
+	cfg *config.JWTConfiguration,
+	privateKey interface{},
+	log *zap.Logger,
+	publicKey interface{},
+	kid string,
+) (string, *rsa.PrivateKey, *rsa.PublicKey) {
+	if len(cfg.RSAPrivateKey) > 0 {
+		privateKey = []byte(cfg.RSAPrivateKey)
+	} else if len(cfg.RSAPRivateKeyFile) > 0 {
+		content, err := os.ReadFile(cfg.RSAPRivateKeyFile)
+		if err != nil {
+			log.Fatal("Could not load key file", zap.String("file", cfg.RSAPRivateKeyFile), zap.Error(err))
+		}
+		if len(content) == 0 {
+			log.Fatal("Read empty private key file", zap.String("file", cfg.RSAPRivateKeyFile), zap.Error(err))
+		}
+		privateKey = content
+	} else {
+		log.Fatal("No RSA private key defined, either set jwt.rsa-private-key or jwt.rsa-private-key-file")
+	}
+	var err error
+	privateKey, err = parseRSAPrivateKey(privateKey.([]byte))
+	if err != nil {
+		log.Fatal("Unable to process suplied private key", zap.Error(err))
+	}
+	if len(cfg.RSAPublicKey) > 0 {
+		publicKey = []byte(cfg.RSAPublicKey)
+	} else if len(cfg.RSAPRivateKeyFile) > 0 {
+		content, err := os.ReadFile(cfg.RSAPublicKeyFile)
+		if err != nil {
+			log.Fatal("Could not load key file", zap.String("file", cfg.RSAPublicKeyFile), zap.Error(err))
+		}
+		publicKey = content
+	} else {
+		log.Fatal("No RSA private key defined, either set jwt.rsa-public-key or jwt.rsa-public-key-file")
+	}
+	kid = fmt.Sprintf("%x", crc32.Checksum(publicKey.([]byte), crc32.IEEETable))
+	pubParsed, err := parseRSAPublicKey(publicKey.([]byte))
+	if err != nil {
+		log.Fatal("Unable to process supllied public key", zap.Error(err))
+	}
+	priv := privateKey.(*rsa.PrivateKey)
+	priv.PublicKey = *pubParsed
+	return kid, priv, pubParsed
+}
+
+func loadHMACKey(
+	cfg *config.JWTConfiguration,
+	log *zap.Logger, privateKey interface{},
+	privateKeyJwk jwk.Key,
+	options []jwt.ParseOption) []jwt.ParseOption {
+	//direct key takes precende
+	if len(cfg.HMACSigningKey) > 0 {
+		checkForWeakHMAC(log, cfg.Algorithm, cfg.HMACSigningKey)
+		privateKey = []byte(cfg.HMACSigningKey)
+	} else if len(cfg.HMACSigningKeyFile) > 0 {
+		content, err := os.ReadFile(cfg.HMACSigningKeyFile)
+		if err != nil {
+			log.Fatal("Could not load key file", zap.String("file", cfg.HMACSigningKeyFile), zap.Error(err))
+		}
+		checkForWeakHMAC(log, cfg.Algorithm, string(content))
+
+	} else {
+		log.Fatal("No HMAC key defined, either set jwt.hmac-signing-key or jwt.hmac-signing-key-file")
+	}
+	if len(privateKey.([]byte)) > 0 {
+		var err error
+		privateKeyJwk, err = jwk.FromRaw(privateKey)
+		if err != nil {
+			log.Fatal("Unable to process symetric key")
+		}
+		options = append(
+			options,
+			jwt.WithKey(jwa.SignatureAlgorithm(cfg.Algorithm), privateKeyJwk),
+		)
+	}
+	return options
 }
 
 func (t *TokenIssuer) Audience() []string {

.conform.yaml
@@ -3,16 +3,18 @@ policies:
     spec:
       header:
         length: 89
-        imperative: true
         case: lower
         invalidLastCharacters: .
-      body:
-        required: true
       spellcheck:
         locale: US
       conventional:
         types:
-          - "type"
-        scopes:
-          - "scope"
+          - "ci"
+          - "docs"
+          - "refactor"
+          - "perf"
+          - "feat"
+          - "fix"
+          - "chore"
+          - "test"
         descriptionLength: 72

Taskfile.yaml
@@ -82,7 +82,7 @@ tasks:
   lint:golines:
     desc: "Run golines"
     cmds:
-      - golines -w -m 200 . 
+      - golines -w -m 100 . 
     silent: true
 
   templates:dev:

api/app/account/account_signin.go
@@ -31,17 +31,7 @@ func (a *AccountRessource) signin(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		if errors.Is(user.ErrEntityOperationForbidden, err) {
 			//locked or unconfirmed
-			err := a.loginTmpl.Execute(w, map[string]interface{}{
-				"i18n":           a.getTranslatorFor(r.Context(), "signin"),
-				"returnUrl":      returnURL,
-				"error":          "locked_user",
-				"otp":            false,
-				"email":          email,
-				csrf.TemplateTag: csrf.TemplateField(r),
-			})
-			if err != nil {
-				a.log.Error("unable to render template for login page", zap.Error(err))
-			}
+			a.renderLoginErrorView(w, r, returnURL, email, "locked_user", false)
 			return
 		}
 		if errors.Is(user.ErrMFARequired, err) {
@@ -50,18 +40,7 @@ func (a *AccountRessource) signin(w http.ResponseWriter, r *http.Request) {
 				a.log.Error("unable prepare MFA", zap.Error(err))
 			}
 			//mfa
-			err := a.loginTmpl.Execute(w, map[string]interface{}{
-				"i18n":           a.getTranslatorFor(r.Context(), "signin"),
-				"returnUrl":      returnURL,
-				"error":          "mfa",
-				"otp":            true,
-				"email":          email,
-				"password":       password,
-				csrf.TemplateTag: csrf.TemplateField(r),
-			})
-			if err != nil {
-				a.log.Error("unable to render template for login page", zap.Error(err))
-			}
+			a.renderLoginErrorView(w, r, returnURL, email, "mfa", true)
 			return
 		}
 		if errors.Is(user.ErrInvalidOTP, err) {
@@ -69,61 +48,19 @@ func (a *AccountRessource) signin(w http.ResponseWriter, r *http.Request) {
 			if err != nil {
 				a.log.Error("unable prepare MFA", zap.Error(err))
 			}
-			//mfa
-			err := a.loginTmpl.Execute(w, map[string]interface{}{
-				"i18n":           a.getTranslatorFor(r.Context(), "signin"),
-				"returnUrl":      returnURL,
-				"error":          "invalid_otp",
-				"otp":            true,
-				"email":          email,
-				"password":       password,
-				csrf.TemplateTag: csrf.TemplateField(r),
-			})
-			if err != nil {
-				a.log.Error("unable to render template for login page", zap.Error(err))
-			}
+			a.renderLoginErrorView(w, r, returnURL, email, "invalid_otp", true)
+
 			return
 		}
 		if errors.Is(user.ErrEntityDoesNotExist, err) {
-			err := a.loginTmpl.Execute(w, map[string]interface{}{
-				"i18n":           a.getTranslatorFor(r.Context(), "signin"),
-				"returnUrl":      returnURL,
-				"otp":            false,
-				"error":          "unknown_or_invalid",
-				"email":          email,
-				csrf.TemplateTag: csrf.TemplateField(r),
-			})
-			if err != nil {
-				a.log.Error("unable to render template for login page", zap.Error(err))
-			}
+			a.renderLoginErrorView(w, r, returnURL, email, "unknown_or_invalid", false)
 			return
 		}
 		if errors.Is(user.ErrInvalidCredentials, err) {
-			err := a.loginTmpl.Execute(w, map[string]interface{}{
-				"i18n":           a.getTranslatorFor(r.Context(), "signin"),
-				"returnUrl":      returnURL,
-				"email":          email,
-				"otp":            false,
-				"error":          "unknown_or_invalid",
-				csrf.TemplateTag: csrf.TemplateField(r),
-			})
-			if err != nil {
-				a.log.Error("unable to render template for login page", zap.Error(err))
-			}
+			a.renderLoginErrorView(w, r, returnURL, email, "unknown_or_invalid", false)
 			return
 		}
-
-		err := a.loginTmpl.Execute(w, map[string]interface{}{
-			"i18n":           a.getTranslatorFor(r.Context(), "signin"),
-			"returnUrl":      returnURL,
-			"email":          email,
-			"otp":            false,
-			"error":          "unknown",
-			csrf.TemplateTag: csrf.TemplateField(r),
-		})
-		if err != nil {
-			a.log.Error("unable to render template for login page", zap.Error(err))
-		}
+		a.renderLoginErrorView(w, r, returnURL, email, "unknown", false)
 		//everything below here is unexepcted
 		a.log.Info("failed signin due to unexpected error", zap.Error(err))
 		return
@@ -150,6 +87,27 @@ func (a *AccountRessource) signin(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Redirect(w, r, returnURL, http.StatusFound)
 
+}
+
+func (a *AccountRessource) renderLoginErrorView(
+	w http.ResponseWriter,
+	r *http.Request,
+	returnURL string,
+	email string,
+	errorString string,
+	otp bool,
+) {
+	err := a.loginTmpl.Execute(w, map[string]interface{}{
+		"i18n":           a.getTranslatorFor(r.Context(), "signin"),
+		"returnUrl":      returnURL,
+		"error":          errorString,
+		"otp":            otp,
+		"email":          email,
+		csrf.TemplateTag: csrf.TemplateField(r),
+	})
+	if err != nil {
+		a.log.Error("unable to render template for login page", zap.Error(err))
+	}
 }
 
 func (a *AccountRessource) signinPage(w http.ResponseWriter, r *http.Request) {

api/app/account/account_signup.go
@@ -19,35 +19,14 @@ func (a *AccountRessource) signup(w http.ResponseWriter, r *http.Request) {
 	phone := r.FormValue("phone")
 	password := r.FormValue("password")
 	if email == "" || !emailRegex.MatchString(email) {
-		err := a.signUpTmpl.Execute(w, map[string]interface{}{
-			"i18n":             a.getTranslatorFor(r.Context(), "signup"),
-			"error":            "invalid_email",
-			"show_invite_code": invite != "",
-			"email":            email,
-			"password":         password,
-			"invite_code":      invite,
-
-			csrf.TemplateTag: csrf.TemplateField(r),
-		})
-		if err != nil {
-			a.log.Error("unable to render template for signup page", zap.Error(err))
-		}
+		a.renderSignupError(w, r, invite, email, password, "invalid_email")
 		return
 	}
 
 	if invite == "" && a.cfg.InviteOnly {
-		err := a.signUpTmpl.Execute(w, map[string]interface{}{
-			"i18n":             a.getTranslatorFor(r.Context(), "signup"),
-			"error":            "invite_code_required",
-			"show_invite_code": true,
-			"email":            email,
-			"password":         password,
-			csrf.TemplateTag:   csrf.TemplateField(r),
-		})
-		if err != nil {
-			a.log.Error("unable to render template for signup page", zap.Error(err))
-		}
+		a.renderSignupError(w, r, invite, email, password, "invite_code_required")
 		return
+
 	}
 	var phoneNr *string
 	if phone != "" {
@@ -56,18 +35,7 @@ func (a *AccountRessource) signup(w http.ResponseWriter, r *http.Request) {
 	if invite != "" {
 		_, err = a.userService.RegisterFromInvite(r.Context(), email, password, phoneNr, invite)
 		if errors.Is(user.ErrTokenExpired, err) {
-			err := a.signUpTmpl.Execute(w, map[string]interface{}{
-				"i18n":             a.getTranslatorFor(r.Context(), "signup"),
-				"error":            "invite_code_expired",
-				"show_invite_code": true,
-				"invite_code":      invite,
-				"email":            email,
-				"password":         password,
-				csrf.TemplateTag:   csrf.TemplateField(r),
-			})
-			if err != nil {
-				a.log.Error("unable to render template for signup page", zap.Error(err))
-			}
+			a.renderSignupError(w, r, invite, email, password, "invite_code_expired")
 			return
 		}
 
@@ -76,33 +44,11 @@ func (a *AccountRessource) signup(w http.ResponseWriter, r *http.Request) {
 	}
 	if err != nil {
 		if errors.Is(user.ErrPasswordGuidelines, err) {
-			err := a.signUpTmpl.Execute(w, map[string]interface{}{
-				"i18n":             a.getTranslatorFor(r.Context(), "signup"),
-				"error":            "password_guidlines",
-				"show_invite_code": true,
-				"invite_code":      invite,
-				"email":            email,
-				"password":         password,
-				csrf.TemplateTag:   csrf.TemplateField(r),
-			})
-			if err != nil {
-				a.log.Error("unable to render template for signup page", zap.Error(err))
-			}
+			a.renderSignupError(w, r, invite, email, password, "password_guidlines")
 			return
 		}
 		if errors.Is(user.ErrEntityAlreadyExists, err) {
-			err := a.signUpTmpl.Execute(w, map[string]interface{}{
-				"i18n":             a.getTranslatorFor(r.Context(), "signup"),
-				"error":            "email_already_used",
-				"show_invite_code": true,
-				"invite_code":      invite,
-				"email":            email,
-				"password":         password,
-				csrf.TemplateTag:   csrf.TemplateField(r),
-			})
-			if err != nil {
-				a.log.Error("unable to render template for signup page", zap.Error(err))
-			}
+			a.renderSignupError(w, r, invite, email, password, "email_already_used")
 			return
 		}
 		a.log.Error("unexpected user sign up error", zap.Error(err))
@@ -116,6 +62,28 @@ func (a *AccountRessource) signup(w http.ResponseWriter, r *http.Request) {
 		"successful":      true,
 		"success_message": successMessage,
 		csrf.TemplateTag:  csrf.TemplateField(r),
+	})
+	if err != nil {
+		a.log.Error("unable to render template for signup page", zap.Error(err))
+	}
+}
+
+func (a *AccountRessource) renderSignupError(
+	w http.ResponseWriter,
+	r *http.Request,
+	invite string,
+	email string,
+	password string,
+	errorString string,
+) {
+	err := a.signUpTmpl.Execute(w, map[string]interface{}{
+		"i18n":             a.getTranslatorFor(r.Context(), "signup"),
+		"error":            errorString,
+		"show_invite_code": true,
+		"invite_code":      invite,
+		"email":            email,
+		"password":         password,
+		csrf.TemplateTag:   csrf.TemplateField(r),
 	})
 	if err != nil {
 		a.log.Error("unable to render template for signup page", zap.Error(err))

api/app/connect/connect.go
@@ -1,6 +1,7 @@
 package connect
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/eisenwinter/gotrxx/api/auth"
@@ -63,6 +64,30 @@ func (c *ConnnectRessource) Router() *chi.Mux {
 	return r
 }
 
+func (c *ConnnectRessource) fieldValidationNotEmpty(
+	w http.ResponseWriter,
+	r *http.Request,
+	name string,
+	value string,
+) bool {
+	if value == "" {
+		err := render.Render(
+			w,
+			r,
+			createStdError(
+				stdInvalidRequest,
+				http.StatusBadRequest,
+				fmt.Sprintf("field not supplied: %s", name),
+			),
+		)
+		if err != nil {
+			c.logger.Error("unable to render response", zap.Error(err))
+		}
+		return false
+	}
+	return true
+}
+
 func (c *ConnnectRessource) token(w http.ResponseWriter, r *http.Request) {
 	//issues accessTokenResponse
 	err := r.ParseForm()
@@ -78,51 +103,16 @@ func (c *ConnnectRessource) token(w http.ResponseWriter, r *http.Request) {
 	switch grantType(grant) {
 	case passwordGrant:
 		username := r.FormValue("username")
-		if username == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(
-					stdInvalidRequest,
-					http.StatusBadRequest,
-					"username field not supplied",
-				),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+		if !c.fieldValidationNotEmpty(w, r, "username", username) {
 			return
 		}
 		password := r.FormValue("password")
-		if password == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(
-					stdInvalidRequest,
-					http.StatusBadRequest,
-					"password field not supplied",
-				),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+		if !c.fieldValidationNotEmpty(w, r, "password", password) {
 			return
 		}
 		clientID := r.FormValue("client_id")
-		if clientID == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(
-					stdInvalidRequest,
-					http.StatusBadRequest,
-					"client_id field not supplied",
-				),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+
+		if !c.fieldValidationNotEmpty(w, r, "client_id", clientID) {
 			return
 		}
 		clientSecret := r.FormValue("client_secret")
@@ -140,19 +130,7 @@ func (c *ConnnectRessource) token(w http.ResponseWriter, r *http.Request) {
 		return
 	case refreshTokenGrant:
 		refreshToken := r.FormValue("refresh_token")
-		if refreshToken == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(
-					stdInvalidRequest,
-					http.StatusBadRequest,
-					"refresh_token field not supplied",
-				),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+		if !c.fieldValidationNotEmpty(w, r, "refresh_token", refreshToken) {
 			return
 		}
 		// client id is NOT required for a refresh_token grant see
@@ -205,36 +183,14 @@ func (c *ConnnectRessource) token(w http.ResponseWriter, r *http.Request) {
 			clientID = id
 			clientSecret = suppliedSecret
 		}
-		if clientID == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(
-					stdInvalidRequest,
-					http.StatusBadRequest,
-					"client_id field not supplied",
-				),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+		if !c.fieldValidationNotEmpty(w, r, "client_id", clientID) {
 			return
 		}
-		if clientSecret == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(
-					stdInvalidRequest,
-					http.StatusBadRequest,
-					"client_secret field not supplied",
-				),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+
+		if !c.fieldValidationNotEmpty(w, r, "client_secret", clientSecret) {
 			return
 		}
+
 		scope := r.FormValue("scope")
 		req := &clientCredentialsTokenRequest{
 			clientID:     clientID,
@@ -247,49 +203,21 @@ func (c *ConnnectRessource) token(w http.ResponseWriter, r *http.Request) {
 		//https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.3
 		c.logger.Debug("auth code")
 		code := r.FormValue("code")
-		if code == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(stdInvalidRequest, http.StatusBadRequest, "code field not supplied"),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+
+		if !c.fieldValidationNotEmpty(w, r, "code", code) {
 			return
 		}
+
 		redirectURI := r.FormValue("redirect_uri")
-		if redirectURI == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(
-					stdInvalidRequest,
-					http.StatusBadRequest,
-					"redirect_uri field not supplied",
-				),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+		if !c.fieldValidationNotEmpty(w, r, "redirect_uri", redirectURI) {
 			return
 		}
+
 		clientID := r.FormValue("client_id")
-		if clientID == "" {
-			err = render.Render(
-				w,
-				r,
-				createStdError(
-					stdInvalidRequest,
-					http.StatusBadRequest,
-					"client_id field not supplied",
-				),
-			)
-			if err != nil {
-				c.logger.Error("unable to render response", zap.Error(err))
-			}
+		if !c.fieldValidationNotEmpty(w, r, "client_id", clientID) {
 			return
 		}
+
 		clientSecret := r.FormValue("client_secret")
 		codeVerifier := r.FormValue("code_verifier")
 		if codeVerifier == "" && clientSecret == "" {

api/app/connect/connect_introspect.go
@@ -12,7 +12,6 @@ import (
 
 // https://datatracker.ietf.org/doc/html/rfc7662
 func (c *ConnnectRessource) introspect(w http.ResponseWriter, r *http.Request) {
-
 	err := r.ParseForm()
 	if err != nil {
 		c.logger.Error("error on parsing form in retrospection endpoint", zap.Error(err))
@@ -22,7 +21,6 @@ func (c *ConnnectRessource) introspect(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
 	ai, err := auth.FromContext(r.Context())
 	if err != nil {
 		err = render.Render(

db/store_auth.go
@@ -31,7 +31,15 @@ func (d *DataStore) Authorizations(
 
 	var entities []*tables.AuthorizationTable
 	q := sq.
-		Select("id", "application_id", "user_id", "properties", "revoked_at", "created_at", "updated_at").
+		Select(
+			"id",
+			"application_id",
+			"user_id",
+			"properties",
+			"revoked_at",
+			"created_at",
+			"updated_at",
+		).
 		From("authorizations").
 		OrderBy("id DESC").Offset(uint64(offset)).Limit(uint64(opts.PageSize))
 	err = d.selectStatement(ctx, &entities, q, nil)

commit 01a6d9fea266448e1de2fbb7f23fc4c3190319b8

fix: broken key loading introduced in a9cca51c9bb2e4e5fb5243f42ab1d78d4bdf48a5

tokens/issuer.go
@@ -150,8 +150,6 @@ func NewIssuer(
 	cfg *config.JWTConfiguration,
 	storage CommonTokenInserter,
 ) *TokenIssuer {
-	var privateKey interface{}
-	var publicKey interface{}
 
 	var privateKeyJwk jwk.Key
 	var publicKeyJwk jwk.Key
@@ -161,11 +159,10 @@ func NewIssuer(
 	//okay this is probably the only reason and place to panic...
 	switch cfg.Algorithm {
 	case "HS256", "HS384", "HS512":
-		options = loadHMACKey(cfg, log, privateKey, privateKeyJwk, options)
+		privateKeyJwk, options = loadHMACKey(cfg, log, options)
 	case "RS256", "RS384", "RS512":
 		var err error
-		var pubParsed *rsa.PublicKey
-		kid, privateKey, pubParsed = loadRSAKeys(cfg, privateKey, log, publicKey, kid)
+		kid, privateKey, pubParsed := loadRSAKeys(cfg, log, kid)
 		privateKeyJwk, err = jwk.FromRaw(privateKey)
 		if err != nil {
 			log.Fatal("Unable to process private key")
@@ -213,11 +210,11 @@ func NewIssuer(
 
 func loadRSAKeys(
 	cfg *config.JWTConfiguration,
-	privateKey interface{},
 	log *zap.Logger,
-	publicKey interface{},
 	kid string,
 ) (string, *rsa.PrivateKey, *rsa.PublicKey) {
+	var privateKey interface{}
+	var publicKey interface{}
 	if len(cfg.RSAPrivateKey) > 0 {
 		privateKey = []byte(cfg.RSAPrivateKey)
 	} else if len(cfg.RSAPRivateKeyFile) > 0 {
@@ -260,9 +257,9 @@ func loadRSAKeys(
 
 func loadHMACKey(
 	cfg *config.JWTConfiguration,
-	log *zap.Logger, privateKey interface{},
-	privateKeyJwk jwk.Key,
-	options []jwt.ParseOption) []jwt.ParseOption {
+	log *zap.Logger,
+	options []jwt.ParseOption) (jwk.Key, []jwt.ParseOption) {
+	var privateKey interface{}
 	//direct key takes precende
 	if len(cfg.HMACSigningKey) > 0 {
 		checkForWeakHMAC(log, cfg.Algorithm, cfg.HMACSigningKey)
@@ -273,13 +270,14 @@ func loadHMACKey(
 			log.Fatal("Could not load key file", zap.String("file", cfg.HMACSigningKeyFile), zap.Error(err))
 		}
 		checkForWeakHMAC(log, cfg.Algorithm, string(content))
+		privateKey = content
 
 	} else {
 		log.Fatal("No HMAC key defined, either set jwt.hmac-signing-key or jwt.hmac-signing-key-file")
 	}
 	if len(privateKey.([]byte)) > 0 {
 		var err error
-		privateKeyJwk, err = jwk.FromRaw(privateKey)
+		privateKeyJwk, err := jwk.FromRaw(privateKey)
 		if err != nil {
 			log.Fatal("Unable to process symetric key")
 		}
@@ -287,8 +285,10 @@ func loadHMACKey(
 			options,
 			jwt.WithKey(jwa.SignatureAlgorithm(cfg.Algorithm), privateKeyJwk),
 		)
+		return privateKeyJwk, options
 	}
-	return options
+	log.Fatal("No HMAC key defined, either set jwt.hmac-signing-key or jwt.hmac-signing-key-file")
+	panic("no valid key found")
 }
 
 func (t *TokenIssuer) Audience() []string {