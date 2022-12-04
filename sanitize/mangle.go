a9c1371e1bf88e9efe961d1d00042e4f71357f05
Jan <jan@subkonstrukt.at>
2022-12-04 13:33:10 +0100

    refactor: use ReplaceAll

--- sanitize/mangle.go
+++ sanitize/mangle.go
@@ -10,14 +10,13 @@ import (
 // UserInputString is used to strip value of any \r \n to
 // avoiding log injection / CWE-117
 func UserInputString(key string, value string) zapcore.Field {
-	esc := strings.Replace(value, "\n", "", -1)
-	esc = strings.Replace(esc, "\r", "", -1)
+	esc := NoLineBreaks(value)
 	return zap.String(key, esc)
 }
 
 // NoLineBreaks removes linebreaks and carrage returns from string
 func NoLineBreaks(value string) string {
-	esc := strings.Replace(value, "\n", "", -1)
-	esc = strings.Replace(esc, "\r", "", -1)
+	esc := strings.ReplaceAll(value, "\n", "")
+	esc = strings.ReplaceAll(esc, "\r", "")
 	return esc
 }
